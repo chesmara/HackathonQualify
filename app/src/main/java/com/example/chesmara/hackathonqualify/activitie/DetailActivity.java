494b11acc94e122c67e457d03edc435055d5489c
chesmara <srdjan@ekoneimar.com>
2017-05-23 12:11:42 +0200

    DetailActivity and ArticleActivity

--- app/src/main/java/com/example/chesmara/hackathonqualify/activitie/DetailActivity.java
+++ app/src/main/java/com/example/chesmara/hackathonqualify/activitie/DetailActivity.java
@@ -1,20 +1,32 @@
 package com.example.chesmara.hackathonqualify.activitie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.example.chesmara.hackathonqualify.R;
 import com.example.chesmara.hackathonqualify.dbase.DatabaseHelper;
+import com.example.chesmara.hackathonqualify.dbase.model.Article;
 import com.example.chesmara.hackathonqualify.dbase.model.User;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 
 import java.sql.SQLException;
+import java.util.List;
+//-----------------------------------------------------------------------------------------
+//---------------------------user data and articles----------------------------------------
+//-----------------------------------------------------------------------------------------
 
 public class DetailActivity extends AppCompatActivity {
 
     private DatabaseHelper databaseHelper;
     private User u;
+    private static final String ARTICLE_KEY_DETAIL="article key detail";
 
     private EditText uName;
     private EditText uAdress;
@@ -26,8 +38,9 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
 
-        int userConnection = getIntent().getExtras().getInt(MainActivity.ARTICLE_KEY);
+        int userConnection = getIntent().getExtras().getInt(MainActivity.USER_ID);
 
+        //------------------------------USER info--------------------------------------
         try {
             u=getDatabaseHelper().getmUserDao().queryForId(userConnection);
 
@@ -38,6 +51,37 @@ public class DetailActivity extends AppCompatActivity {
             uName.setText(u.getuName());
             uAdress.setText(u.getuAdress());
             uEmail.setText(u.getuEmail());
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //-----------------------------user article list---------------------------------
+
+       final  ListView userArticlesList = (ListView) findViewById(R.id.user_articles_list);
+
+        List<Article> ArtList= null;
+        try {
+            ArtList = getDatabaseHelper().getmArticleDao().queryBuilder()
+                    .where()
+                    .eq(Article.FIELD_NAME_USER, u.getuId())
+                    .query();
+
+            ListAdapter artadapter = new ArrayAdapter<>(this,R.layout.list_item, ArtList);
+            userArticlesList.setAdapter(artadapter);
+
+            userArticlesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Article ar= (Article) userArticlesList.getItemAtPosition(position);
+                    Intent intent = new Intent(DetailActivity.this,ArticleActivity.class);
+                    intent.putExtra(ARTICLE_KEY_DETAIL, ar.getaId());
+                    startActivity(intent);
+                }
+            });
+
+
 
 
         } catch (SQLException e) {
